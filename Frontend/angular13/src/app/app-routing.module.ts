016cb273cec8dc7a7fe17cf355054607078afd31
Razo <razonir@gmail.com>
2022-01-23 11:10:39 +0200

    form

--- Frontend/angular13/src/app/app-routing.module.ts
+++ Frontend/angular13/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component'
 import { CryptoComponent } from './screens/crypto/crypto.component'
+import { LoginComponent } from './screens/LoginFolder/login/login.component'
+import { RegisterComponent } from './screens/LoginFolder/register/register.component'
 
-const routes: Routes = [{path: 'HomeComponent', component: HomeComponent},
+const routes: Routes = [
+{path: '', component: HomeComponent},
 {path: 'CryptoComponent', component: CryptoComponent},
-{path: '**', component: HomeComponent}
+{path: 'login', component: LoginComponent},
+{path: 'register', component: RegisterComponent},
+{path: '**', redirectTo: ""}
 ];
 
 @NgModule({
--- Frontend/angular13/src/app/app.module.ts
+++ Frontend/angular13/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import {HttpClientModule} from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { BodyComponent } from './layout/body/body.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { CryptoComponent } from './screens/crypto/crypto.component';
 import { HomeComponent } from './screens/home/home.component';
+import { LoginComponent } from './screens/LoginFolder/login/login.component';
+import { RegisterComponent } from './screens/LoginFolder/register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,15 +21,16 @@ import { HomeComponent } from './screens/home/home.component';
     BodyComponent,
     FooterComponent,
     CryptoComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+    FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
